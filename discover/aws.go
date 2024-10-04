baa3827998ae8cec459d68418af3dc62cf05f1c7
Robert Van Ysendyck <98933151+robertvy@users.noreply.github.com>
2024-10-04 18:35:07 +0200

    Update aws.go

--- discover/aws.go
+++ discover/aws.go
@@ -2,12 +2,14 @@ package discover
 
 import (
 	"net"
+	"net/http"
+	"time"
 )
 
 const (
-	awsPrivateIPv4URL = "http://169.254.169.254/latest/meta-data/local-ipv4"
-	awsPublicIPv4URL  = "http://169.254.169.254/latest/meta-data/public-ipv4"
-	awsHostnameURL    = "http://169.254.169.254/latest/meta-data/public-hostname"
+	awsMetadataTokenURL = "http://169.254.169.254/latest/api/token"
+	awsMetadataBaseURL  = "http://169.254.169.254/latest/meta-data"
+	tokenTTL            = "21600" // Token time-to-live in seconds (6 hours)
 )
 
 // NewAWSDiscoverer returns a new Amazon Web Services network discoverer
@@ -17,17 +19,47 @@ func NewAWSDiscoverer() Discoverer {
 		PublicIPv4DiscovererOption(awsPublicIPv4),
 		PublicHostnameDiscovererOption(awsHostname),
 	)
+}
 
+func getMetadataToken() (string, error) {
+	client := &http.Client{Timeout: 5 * time.Second}
+	req, err := http.NewRequest("PUT", awsMetadataTokenURL, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", tokenTTL)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	token, err := StandardResponseBodyString(resp)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func getMetadata(path string) (string, error) {
+	token, err := getMetadataToken()
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{Timeout: 5 * time.Second}
+	req, err := http.NewRequest("GET", awsMetadataBaseURL+path, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("X-aws-ec2-metadata-token", token)
+
+	return StandardStringFromHTTP(req, client)
 }
 
 func awsPrivateIPv4() (net.IP, error) {
-	return StandardIPFromHTTP(awsPrivateIPv4URL, nil)
-}
-
-func awsPublicIPv4() (net.IP, error) {
-	return StandardIPFromHTTP(awsPublicIPv4URL, nil)
-}
-
-func awsHostname() (string, error) {
-	return StandardHostnameFromHTTP(awsHostnameURL, nil)
-}
+	ipString, err := getMetadata("/local-ipv4")
+	if err != nil {
+		return nil,
